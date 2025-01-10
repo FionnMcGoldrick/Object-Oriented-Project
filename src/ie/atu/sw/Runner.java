04e444ff6c0d2685ccb8d9968e6272d46de23651
FIONN MC GOLDRICK <G00422349@atu.ie>
2025-01-10 12:19:24 +0000

    Final commit. Project working as intended.

--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -2,6 +2,11 @@ package ie.atu.sw;
 
 public class Runner {
 
+    /*
+    GitHub Repository Link:
+    https://github.com/FionnMcGoldrick/Object-Oriented-Project
+     */
+
     public static void main(String[] args) {
 
         //Create a new console object to start the application
